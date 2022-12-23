85b5c7638afa53f9c7b60f6fe9afe048db0a4fd4
almeida1 <engsoftware1@gmail.com>
2022-12-23 11:08:32 -0300

    CRUD exemplo 1  On branch master Changes to be committed: 	modified:   src/App.js 	new file:   src/componentes/EditUser/index.js  Untracked files: 	src/componentes/AddUser/ 	src/componentes/Header/ 	src/componentes/Lista/ 	src/styles.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
+import Header from "./componentes/Header";
+import Lista from "./componentes/Lista";
+import AddUser from "./componentes/AddUser";
+import EditUser from "./componentes/EditUser";
 
-function App() {
+const App = () => {
+  //dados armazenados no componente pai
+  //array de objetos
+  //o unico componente que pode alterar o objeto state
+  //é aquele no qual o state foi definido
+  const usersData = [
+    { id: 1, name: "Tania Silva1", username: "tania" },
+    { id: 2, name: "Craig Larman", username: "craig" },
+    { id: 3, name: "Ben Jonhson", username: "ben" },
+  ];
+  //state é um termo generico para designar um objeto destinado a armazenar dados
+  const [users, setUsers] = useState(usersData);
+  const initialFormState = { id: null, name: "", username: "" };
+  const [editing, setEditing] = useState(false);
+  const [currentUser, setCurrentUser] = useState(initialFormState);
+
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
+
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+  const editRow = (user) => {
+    setEditing(true);
+
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <div className="flex-row">
+        <div className="flex-large">
+          <h4>Cadastrar Usuario</h4>
+          <AddUser addUser={addUser} />
+        </div>
+        <h4>Editar usuário</h4>
+        <EditUser
+          editing={editing}
+          setEditing={setEditing}
+          currentUser={currentUser}
+          updateUser={updateUser}
+        />
+        <div className="flex-large">
+          <h4>Lista de Usuarios</h4>
+          <Lista users={users} editRow={editRow} deleteUser={deleteUser} />
+        </div>
+      </div>
     </div>
   );
-}
-
+};
 export default App;
